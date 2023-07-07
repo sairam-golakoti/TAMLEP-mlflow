9bba34d7d13ad1155b134032309c181d0ce24422
Golakoti G V P Sai Ram <sairam.golakoti@tigeranalytics.com>
2023-07-07 17:09:50 +0530

    ENH: hands-on MLFlow walkthrough

---
+++ train.py
@@ -0,0 +1,69 @@
+import logging
+import sys
+import warnings
+
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+def load_data(data_path):
+    data = pd.read_csv(data_path)
+
+    train, test = train_test_split(data)
+
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    return train_x, train_y, test_x, test_y
+
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    data_path = "data/winequality-red.csv"
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train_x, train_y, test_x, test_y = load_data(data_path)
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predict_qualities = lr.predict(test_x)
+        (rmse, mae, r2) = eval_metrics(test_y, predict_qualities)
+
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_artifact(data_path)
+        print("Save to: {}".format(mlflow.get_artifact_uri()))
+
+        mlflow.sklearn.log_model(lr, "model")
